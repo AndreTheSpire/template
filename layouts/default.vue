f5c1f30fa34fd356568c92b2cb464618371acb66
AndreTheSpire <64310095+AndreTheSpire@users.noreply.github.com>
2023-11-13 11:36:08 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,164 @@
+<template>
+  <v-app class="background-container">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      :mini-variant="miniVariant"
+      @click:mini-variant="miniVariant = !miniVariant"
+      class="sidenav"
+    >
+      <!-- Sidebar content here -->
+        <v-row class="my-5 mx-2">
+      <v-col cols="5" class="d-flex justify-center">
+       <div class="d-flex flex-column justify-center align-center">
+        <div>
+          <v-avatar class="ma-3" size="79">
+            <v-img src="/img/logopfp.png"></v-img>
+          </v-avatar>
+        </div>
+      </div>
+      </v-col>
+      <v-col cols="7" class="d-flex align-center text-header">Sekolah Yoga </v-col>
+    
+    </v-row>
+    <!-- <v-divider
+      :thickness="8"
+      class="border-opacity-100"
+      color="white"
+    ></v-divider> -->
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in routes"
+        :key="index"
+        @click="navigateroute(item,index)"
+        :class="{ 'selected-item': selectedIndex === index }"
+      >
+        <v-icon class="mr-3">{{item.icon}}</v-icon>
+          {{ item.text }}
+        
+      </v-list-item>
+       <v-list-item
+       v-if="$vuetify.display.smAndDown"
+        @click="navigateprofile()"
+        :class="{ 'selected-item': selectedIndex === -1 }"
+      >
+        <v-icon class="mr-3">mdi-account</v-icon>
+          Profile
+        
+      </v-list-item>
+    </v-list>
+
+    </v-navigation-drawer>
+
+    <v-app-bar app v-if="$vuetify.display.smAndDown">
+         <v-app-bar-nav-icon  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    
+    
+      <v-toolbar-title v-if="selectedIndex!=-1" class="text-center text-title">{{routes[selectedIndex].text}}</v-toolbar-title>
+      
+      <v-toolbar-title v-else class="text-center text-title">Profile</v-toolbar-title>
+      <!-- <ProfileHeader v-else :reset="resetindex"/> -->
+      <!-- <NotificationBell /> -->
+    </v-app-bar>
+    <v-app-bar app v-else>
+     <v-app-bar-nav-icon @click.stop="drawer = !drawer">  <v-icon v-if="drawer">mdi-chevron-left</v-icon><v-icon v-else >mdi-chevron-right</v-icon></v-app-bar-nav-icon>
+    
+      <v-toolbar-title class="text-title"  v-if="selectedIndex!=-1">{{routes[selectedIndex].text}}</v-toolbar-title>
+      <v-toolbar-title v-else></v-toolbar-title>
+      <!-- <NotificationBell />
+      <ProfileHeader :reset="resetindex"/> -->
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+
+export default {
+  data() {
+    return {
+      drawer: true, // Controls whether the sidebar is open
+      miniVariant: false, // Controls whether the sidebar is in mini-variant mode (for mobile)
+      routes: [
+        // { text: 'Home', route: '/home', icon:'mdi-home' },
+        { text: 'Evaluasi', route: '/evaluasi',icon:'mdi-clipboard-list-outline' },
+           { text: 'Hasil Evaluasi', route: '/evaluasi/hasilevaluasi',icon:'mdi-file-document-check' },
+        // Add more menu items as needed
+      ],
+      selectedIndex: 0, // Index of the selected item
+
+    }
+  },
+  methods:{
+    resetindex(){
+      console.log("kepanggil");
+      this.selectedIndex=-1;
+      console.log(this.selectedIndex);
+    },
+    toogledrawer(){
+      this.drawer=!this.drawer;
+      console.log(this.drawer);
+      this.toggleSidebar();
+    },
+    navigateroute(route,index) {
+      this.$router.push(route.route);
+      this.selectedIndex = index;
+      
+    },
+    navigateprofile() {
+      this.$router.push("/profile");
+      this.selectedIndex = -1;
+      
+    },
+     toggleSidebar() {
+      this.$emit('toggleSidebar');
+    }, 
+  },mounted(){
+    //  if (this.$cookies.get('loginProfile')) {
+
+
+    //  }else{
+    //   this.$router.push('/');
+    //  }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sidenav{
+  background-color: $primary-color;
+  color: $secondary-color;
+}
+.text-header{
+  font-size: 24px;
+  font-weight: 700;
+  font-family: 'Open Sans';
+  line-height: 32px;
+  color: white;
+}
+.selected-item {
+  background-color:#0D8DDA; /* Change this to your desired background color */
+}
+.background-container{
+  background-color: #F0F0F0;/* Adjust this property as needed */
+}
+.text-title{
+  font-family: "Open Sans";
+font-size: 28px;
+font-style: normal;
+font-weight: 700;
+line-height: 22px;
+}
+@media (max-width: 40em) {
+  .text-title{
+    font-size: 15px;
+  }
+  
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="text-dupe">
+{{profile.profile.fullName}}
+    </div>
+</template>
+
+<script>
+import { ProfileStore } from '../stores/profilestore'
+    export default {
+        data: () => ({
+      profile:ProfileStore(),
+      menuItems: [
+        { title: 'Profile Saya', subtitle:'Lihat profile',icon:'mdi-account' },
+        //  { title: 'Edit Profile', subtitle:'Mengedit profile',icon:'mdi-account-edit' },
+      ],
+    }),
+    props:{
+      reset:Function
+    },
+    methods:{
+      pindahprofile(index){
+        this.reset();
+        this.$router.push('/profile'); 
+      },
+      logout(){
+        this.$router.push('/');
+      }
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+.text-dupe{
+font-family: "Open Sans";
+  font-weight: 500;
+  font-size: 24px;
+  color: black;
+}
+
+</style>
